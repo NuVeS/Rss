0e4c60f25fe6c3d7e7cca3e538560de7537e67e0
Admin <admin@Mac-Admin.local>
2014-09-06 16:59:32 +0400

    Initial Commit

---
+++ Rss/MasterViewController.h
@@ -0,0 +1,13 @@
+//
+//  MasterViewController.h
+//  Rss
+//
+//  Created by Admin on 06.09.14.
+//  Copyright (c) 2014 Amitim. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MasterViewController : UITableViewController
+
+@end
---
+++ Rss/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Rss
+//
+//  Created by Admin on 06.09.14.
+//  Copyright (c) 2014 Amitim. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
